23948d96decd496c1b1cd12179490368e43a0350
stephane <celticbytes@gmail.com>
2026-02-01 16:47:49 +0100

    style: update BEM naming convention regex for class selectors

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,6 @@ export default {
   extends: ['stylelint-config-standard'],
   rules: {
     // Enforce BEM naming convention for class selectors
-    'selector-class-pattern':
-      '^[a-z][a-z0-9]*(?:__(?:[a-z0-9]+))?(?:--[a-z0-9]+)?$',
+    'selector-class-pattern': '^[a-z][a-z0-9-]*(__[a-z0-9-]+)?(--[a-z0-9-]+)?$',
   },
 };
